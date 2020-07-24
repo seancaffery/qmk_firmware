06a6bb0757fbd4581cb4cb26549a963141afc3d0
Sean Caffery <scaffery@billy.local>
2020-07-24 15:25:07 +1000

    Update dox

--- keyboards/ergodox_infinity/keymaps/sean/keymap.c
+++ keyboards/ergodox_infinity/keymaps/sean/keymap.c
@@ -18,12 +18,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |   \  |           | :+1: |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |   \  |           |      |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   T  |   Y  |      |           |   [  |   Y  |   U  |   I  |   O  |   P  |  BKSP  |
+ * | Tab    |   Q  |   W  |   E  |   T  |   Y  |      |           |  ⌘+\ |   Y  |   U  |   I  |   O  |   P  |  BKSP  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|      |           |   ]  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  ESC |  ALT |   \  | LEFT |  L1  |                                       | SPC  |  L1  | DOWN |  UP  |  ENT   |
@@ -44,16 +44,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
         KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_TRNS,
-        KC_ESC ,KC_LALT,KC_BSLS,KC_LEFT,MO(SYMB),
+        KC_ESC ,KC_TRNS,KC_BSLS,KC_LEFT,MO(SYMB),
                                                KC_LCTL, KC_LALT,
                                                         KC_HOME,
                                       KC_LGUI, KC_BSPC, KC_END,
 
         // right hand
              KC_TRNS, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS,
-             KC_LBRC, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
+       LGUI(KC_BSLS), KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
                       KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-             KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+             KC_TRNS, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                           KC_SPC, MO(SYMB), KC_DOWN, KC_UP, KC_ENT,
         KC_RALT,KC_RCTL,
         KC_PGUP,
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbol Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | Version|  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  | F11  |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |      |      |      |      |      |    =   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -75,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |      |       | VOLU |      |      |
  *                                 |      |      |------|       |------|      |      |
- *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |      |       | VOLD |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
@@ -99,8 +99,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, EPRM,
         KC_TRNS, KC_TRNS,
-        KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS
+        KC_VOLU,
+        KC_VOLD, KC_TRNS, KC_TRNS
 ),
 /* Keymap 2: Media and mouse keys
  *
